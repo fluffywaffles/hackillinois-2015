db571855e148eca68b61eaca4cd96ea7acb7dc82
gwcmk <gregwcmkim@gmail.com>
2015-02-28 22:55:01 -0600

    Added updateLinks

--- app/services/jig.js
+++ app/services/jig.js
@@ -8,15 +8,32 @@ var config = require('../../config/config'),
 var Jig = require(config.root + '/app/models/jig')
 
 var wlText = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'i', 'span', 'strong', 'em', 'small', 'a', 'u', 'b', 'li', 'code', 'pre', 'blockquote', 'caption', 'input', 'textarea']; 
+var tagsWithLinks = ['video', 'img', 'source', 'script', 'link', 'a'];
 var model = {};
 var outputObject = {'deps': []}
 var HTTPoptions;
 
-function transformHTML(data){
+function transformHTML(data, url){
 	$ = cheerio.load(data);
 	var head = $('head').html();
 
 	wlText.forEach(addAttr);
+	tagsWithLinks.forEach(function updateLinks(element){
+		var nodes = $(element);
+		nodes.each(fucntion(i, elem){
+			var currentLink;
+			if(elem.name === 'video'&& $(this).is("[poster]")){
+				currentLink = $(this).attr('poster');
+				$(this).attr('poster', url['host'] + '/' + currentLink);
+			}else if((elem.name === 'img' || elem.name === 'source' || elem.name === 'script') && $(this).is("[src]")){
+				currentLink = $(this).attr('src');
+				$(this).attr('src', url['host'] + '/' + currentLink);
+			}else if((elem.name === 'link' || elem.name === 'a') && $(this).is("[href]")){
+				currentLink = $(this).attr('href');
+				$(this).attr('href', url['host'] + '/' + currentLink);
+			}
+		})
+	});
 	var body = $('body').html();
 	outputObject['model'] = model;
 	outputObject['body'] = body;
@@ -78,7 +95,7 @@ function getDeps(rawHTML){
 		var CDNregexp = /^((http|https):)?\/\//;
 		if(CDNregexp.exec(cssPath)){
 			// CDN
-			outputObject['deps'].push({'external': url['host'] + url['pathname']});
+			outputObject['deps']['external'].push(url['host'] + url['pathname']);
 		}else{
 			// local CSS
 			addDeps(HTTPoptions['host'], cssPath);
@@ -90,12 +107,11 @@ function addDeps(host, path){
 	HTTPoptions['host'] = host;
 	HTTPoptions['path'] = path;
 	var dep = request('GET', 'http://' + HTTPoptions['host'] + '/' + HTTPoptions['path']);
-	outputObject['deps'].push({'inline': dep.getBody().toString()});	
+	outputObject['deps']['inline'].push(dep.getBody().toString());	
 }
 
 function main(url) {
-	outputObject = {'deps': []}
-	console.log(url);
+	outputObject = {'deps': {'inline': [], 'external': []}}
 	var inputurl = parse(url, true);
 	HTTPoptions = {
 		host: inputurl['host'],
@@ -106,7 +122,7 @@ function main(url) {
 
 	var res = request('GET', 'http://' + HTTPoptions['host'] + HTTPoptions['path']);
 	var rawHTML = res.getBody().toString();
-	transformHTML(rawHTML);
+	transformHTML(rawHTML, inputurl);
 	getDeps(rawHTML);
 
 	var newJig = new Jig({
